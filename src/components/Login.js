4c2db597b0f2403fb77c5a31ef5a60a20960ffee
Gokul <gokulprathin8@gmail.com>
2021-05-06 23:24:20 +0530

    Changes to UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,8 +11,17 @@ const Login = () => {
                     <Join>
                         Join Now
                     </Join>
+                    <SignIn>Sign In</SignIn>
                 </div>
             </Nav>
+            <Section>
+
+                <Hero>
+                   <h1>Welcome to your professional community</h1>
+                    <img src="/images/login-hero.svg" alt="" />
+                </Hero>
+
+            </Section>
         </Container>
     )
 };
@@ -49,6 +58,72 @@ const Join = styled.a`
     background-color: rgba(0, 0, 0, 0.08);
     color: rgba(0, 0, 0, 0.9);
     text-decoration: none;
+  }
+`;
+const SignIn = styled.a`
+    box-shadow: inset 0 0 0 1px #0a66c2;
+    color: #0a66c2;
+    border-radius: 24px;
+    transition-duration: 167ms;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 40px;
+    padding: 10px 24px;
+    text-align: center;
+    background-color: rgba(0, 0, 0, 0);
+  
+    &:hover {
+      background-color: rgba(112, 181, 249, 0.15);
+      color: #0a66c2;
+      text-decoration: none;
+    }
+`;
+const Section = styled.section`
+  display: flex;
+  align-content: center;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+  @media(max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    @media(max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+  
+  img {
+    z-index: 1;
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+    @media(max-width: 768px) {
+      top: 230px;
+      width: initial;
+      position: initial;
+      height: initial;
+    }
   }
 `;
 
